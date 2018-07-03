0c0fa3c87ababbc3d767b2540d11700325ca540f
eranpeer <eranpe@gmail.com>
2018-07-03 12:53:16 -0700

    cleanup

--- src/test/java/devcraft/parking/TestConfig.java
+++ src/test/java/devcraft/parking/TestConfig.java
@@ -1,15 +1,14 @@
 package devcraft.parking;
 
-import devcraft.parking.jpa.DbConfig;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+
+import devcraft.parking.jpa.DbConfig;
 
 @Configuration
 @EnableAutoConfiguration
 @Import(DbConfig.class)
 public class TestConfig {
-
 
 }
