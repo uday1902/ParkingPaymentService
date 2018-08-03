bd5a2e3f28ee87f0f3f7daccd63038249549a57d
Sonthy <vsonthy@ebay.com>
2018-08-03 09:39:07 -0700

    Moving chain of responsibility to the constructor

--- src/main/java/devcraft/parking/policy/DefaultPaymentPolicyHandler.java
+++ src/main/java/devcraft/parking/policy/DefaultPaymentPolicyHandler.java
@@ -3,14 +3,20 @@ package devcraft.parking.policy;
 import java.time.Instant;
 
 public class DefaultPaymentPolicyHandler implements PaymentPolicy {
-    @Override
-    public int calculatePayment(Instant entryTime, Instant paymentTime) {
+
+    private PaymentPolicy escapePaymentPolicy;
+
+    public DefaultPaymentPolicyHandler(){
         PaymentPolicy dailyPaymentPolicy = new DailyPaymentPolicy();
         PaymentPolicy minPaymentPolicy = new MinPaymentPolicy(dailyPaymentPolicy);
         PaymentPolicy weekendNightPaymentPolicy = new WeekendNightPaymentPolicy(minPaymentPolicy);
         PaymentPolicy maxPaymentPolicy = new MaxPaymentPolicy(weekendNightPaymentPolicy);
         PaymentPolicy weeklyPaymentPolicy = new WeeklyPaymentPolicy(maxPaymentPolicy);
-        EscapePaymentPolicy escapePaymentPolicy = new EscapePaymentPolicy(weeklyPaymentPolicy);
+        escapePaymentPolicy = new EscapePaymentPolicy(weeklyPaymentPolicy);
+    }
+
+    @Override
+    public int calculatePayment(Instant entryTime, Instant paymentTime) {
         return escapePaymentPolicy.calculatePayment(entryTime,paymentTime);
     }
 }
